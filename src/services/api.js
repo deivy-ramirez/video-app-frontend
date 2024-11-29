0a7f53df8f2c6918a62d09a0244f37efe4d7cb5f
Deivy Ramirez <davydwindsor@gmail.com>
2024-11-29 00:10:07 -0500

    0.5

--- src/services/api.js
+++ src/services/api.js
@@ -7,7 +7,7 @@ const api = axios.create({
 export const uploadVideo = async (file) => {
   const formData = new FormData();
   formData.append('video', file);
-  const response = await api.post('/middlewares/upload', formData, {
+  const response = await api.post('/api/videos/upload', formData, {
     headers: { 'Content-Type': 'multipart/form-data' }
   });
   return response.data;
